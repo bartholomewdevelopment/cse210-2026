509fcc25b53bc9d39d8cb46158aacd90fe990863
bartholomewdevelopment <joseph@bartholomewdevelopment.com>
2026-03-12 07:47:28 -0400

    all Week 1 Exercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            int guessCount = 1;
+
+
+            while (guess != magicNumber)
+            {
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again?  yes or no: ");
+            playAgain = Console.ReadLine().ToLower();
+        }    
     }
 }
